c01a28ef7d9434c7c34d63ba6487e26b1e2c37f3
Yicen Liu <liuyicenn@163.com>
2020-09-23 23:07:04 -0500

    homework1.2 try2

--- scripts/summarizeSNaQres.sh
+++ scripts/summarizeSNaQres.sh
@@ -8,10 +8,10 @@ echo CPUtime > ../output/CPUtime.csv
 cd ../log
 for name in *.*
 do
-	echo $name  | cut -f1 -d"." >> ../output/analysis.csv
+    grep -o "rootname for files: .*" $name | cut -f4 -d" " >> ../output/analysis.csv
 	grep -o "hmax = [0-9]*" $name | cut -f3 -d" " >> ../output/hmax.csv
 	temp=$(echo $name  | cut -f1 -d".")
-	grep -o "Elapsed time: [0-9]*" ../out/$temp.out | cut -f3 -d" " >> ../output/CPUtime.csv
+	grep -o "Elapsed time: [0-9]*\.[0-9]*" ../out/$temp.out | cut -f3 -d" " >> ../output/CPUtime.csv
 done
 
 paste -d , ../output/analysis.csv ../output/hmax.csv ../output/CPUtime.csv > ../output/Summary.csv
